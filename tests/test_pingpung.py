6234a3f00911bd6f4036c997c71b34da17f4b779
Josh Price <price.joshuad@gmail.com>
2015-02-26 15:43:01 -0800

    sending last few updates before going home for the day.

--- tests/test_pingpung.py
+++ tests/test_pingpung.py
@@ -5,9 +5,18 @@ from pingpung import pingpung as pp
 
 __author__ = 'Josh Price'
 class TestPingPung(TestCase):
+
     def test_init(self):
-        # Checking properties and variable assignment
+        # Init has no return value, so we're mainly checking properties
         instance = pp.PingPung()
 
-        #Check for tabs dict
         self.assertTrue(hasattr(instance, 'tabs'))
+        self.assertIsInstance(instance.tabs, dict)
+        self.assertTrue(hasattr(instance, 'counter_iter'))
+        self.assertTrue(hasattr(instance, 'tab_button'))
+        self.assertTrue(hasattr(instance, 'ui'))
+        self.assertTrue(hasattr(instance.ui, 'tab_bar'))
+
+    def test_add_tab(self):
+        instance = pp.PingPung()
+        pass
