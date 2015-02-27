8975f9ca2a3ff50c38f30c930c6b0e2086b058ea
Price.JoshuaD@Gmail.com <price.joshuad@gmail.com>
2015-02-27 15:43:15 -0800

    trying to get entry points working

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ exe_build_options = dict(path=sys.path,
 long = "PingPung is intended to fill the niche of a truly multiplatform graphical ping application.  Unlike other desktop ping utilities, it does NOT use your operating system's builtin 'ping' program at all.  Instead, it uses a pure Python 3 library which creates its own socket.  **Because of this, it will require root privileges on many platforms.** If you're uncomfortable with that, there are many other platform-specific graphical ping apps out there which do not require root (because they're just wrappers around the OS's existing ping util)."
 
 setup(name='PingPung',
-      version="0.1.4",
+      version="0.1.5",
       author="Josh Price",
       author_email="Price.JoshuaD@Gmail.com",
       description='Python3/QT4 Multiplatform Ping Application',
       license="GPLv2",
       url = 'https://github.com/RainDogSoftware/pingpung/',
-      download_url = 'https://github.com/RainDogSoftware/pingpung/tarball/0.1.3',
+      download_url = 'https://github.com/RainDogSoftware/pingpung/tarball/0.1.5',
       keywords="ping networking network testing",
       packages=["pingpung",
                 "pingpung.pplib"],
@@ -42,12 +42,7 @@ setup(name='PingPung',
                    ),
       long_description=long,
       executables=executables,
-      entry_points={
-
-        'gui_scripts': [
-            'pingpung = pingpung',
-        ]
-    }
+      entry_points={'gui_scripts': ['pingpung = pingpung']},
       classifiers=[
       "Development Status :: 3 - Alpha",
       "Topic :: Utilities",
