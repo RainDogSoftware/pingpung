d1c243971cfcd77251ae368a09ff73456a83e901
Price.JoshuaD@Gmail.com <price.joshuad@gmail.com>
2015-03-20 22:00:13 -0700

    So it turns out the packaging bug I've been chasing isn't a packaging bug, it was an installation bug on my part. Ready for release

--- shared.py
+++ shared.py
@@ -2,13 +2,13 @@
 
 name = 'PingPung'
 long = "PingPung is intended to fill the niche of a truly multiplatform graphical ping application.  Unlike other desktop ping utilities, it does NOT use your operating system's builtin 'ping' program at all.  Instead, it uses a pure Python 3 library which creates its own socket.  **Because of this, it will require root privileges on many platforms.** If you're uncomfortable with that, there are many other platform-specific graphical ping apps out there which do not require root (because they're just wrappers around the OS's existing ping util)."
-version="0.1.11"
+version="0.1.13"
 author="Josh Price"
 author_email="Price.JoshuaD@Gmail.com"
 description='Python3/QT4 Multiplatform Ping Application'
 pplicense="GPLv2"
 url = 'https://github.com/RainDogSoftware/pingpung/'
-download_url = 'https://github.com/RainDogSoftware/pingpung/tarball/0.1.11'
+download_url = 'https://github.com/RainDogSoftware/pingpung/tarball/0.1.13'
 keywords="ping networking network testing"
 packages=["pingpung",
           "pingpung.pplib"]
